10b6bb397c11f34e3e3909b6eba1299fac23c740
Hello! I’m proxyproxyproxy <159737858+proxyproxyproxy-scratch@users.noreply.github.com>
2024-11-16 15:41:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 myY=0;
 function myScroll(){
   window.scroll(0,myY);
-  myY=myY+75;
+  myY=myY+300;
   setTimeout("myScroll()",25);
-  if (myY==12000) {document.location = "main.text.download";};
+  if (myY==48000) {document.location = "main.text.download";};
 }
 // -->
